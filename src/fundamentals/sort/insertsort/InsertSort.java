edb381049d49bed1b197133fa7626da7b91a1129
sssvip <admin@dxscx.com>
2017-01-20 14:34:11 +0800

    chore: add print out instructions

--- src/fundamentals/sort/insertsort/InsertSort.java
+++ src/fundamentals/sort/insertsort/InsertSort.java
@@ -32,15 +32,15 @@ public class InsertSort extends Sortable {
     InsertSort insertSort = new InsertSort();
     //sort from 0 to integers.length
     Integer[] integers = new Integer[] {1, 2, 4, 1, 43, 21, 4, 5, 3};
-    insertSort.show(integers);
+    insertSort.show(integers);//1 2 4 1 43 21 4 5 3
     insertSort.sort(integers);
-    insertSort.show(integers);
+    insertSort.show(integers);//1 1 2 3 4 4 5 21 43
 
     //sort from 3 to 5
     Double[] doubles = new Double[] {1.2, 2.1, 4.4, 33.2, 44.1, 41.3, 12.1, 434.2};
-    insertSort.show(doubles);
+    insertSort.show(doubles);//1.2 2.1 4.4 33.2 44.1 41.3 12.1 434.2
     insertSort.sort(doubles, 3, 5);
-    insertSort.show(doubles);
+    insertSort.show(doubles);//1.2 2.1 4.4 12.1 33.2 41.3 44.1 434.2
 
   }
 }
