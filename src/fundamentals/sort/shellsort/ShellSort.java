1b5231778d56c31795d52e96002bb2eb55bed710
sssvip <admin@dxscx.com>
2017-01-20 15:56:10 +0800

    feat: change a[j-1] to a[j-h] improve the performance

--- src/fundamentals/sort/shellsort/ShellSort.java
+++ src/fundamentals/sort/shellsort/ShellSort.java
@@ -14,20 +14,18 @@ public class ShellSort extends Sortable {
     int size = toIndex - fromIndex;//sort size
     int width = 3;//divides width,it can be try to find the best value
     int h = 1;//
-
     while (h < (size / width)) {
       h = h * width + 1;//1,4,13,40....
     }
 
     while (h >= 1) { //cautious,must >=1
       for (int i = fromIndex; i < toIndex; i += h) {//cautious i+=h, just the divide thinking
-        for (int j = i; j > 0 && less(a[j], a[j - 1]); j -= h) {//use insert sort thinking to sort
-          exch(a, j, j - 1);
+        for (int j = i; j > 0 && less(a[j], a[j - h]); j -= h) {//use insert sort thinking to sort
+          exch(a, j, j - h);
         }
       }
       h = h / width;//....40,13,4,1
     }
-
   }
 
   /**
@@ -37,9 +35,9 @@ public class ShellSort extends Sortable {
    */
   public static void main(String[] args) {
     ShellSort shellSort = new ShellSort();
-    Integer[] integers = new Integer[] {1, 2, 1, 45, 32, 52, 12, 11, 22, 322, 4, 5, 1, 3, 3, 3, 2, 2};
-    shellSort.show(integers);//1 2 1 45 32 52 12 11 22 322 4 5 1 3 3 3 2 2
+    Integer[] integers = new Integer[] {1, 2, 1, 45, 32, 52, 12, 11, 22, 322, 4, 42, 3, 45, 64, 45, 4};
+    shellSort.show(integers);//1 2 1 45 32 52 12 11 22 322 4 42 3 45 64 45 4
     shellSort.sort(integers);
-    shellSort.show(integers);//1 1 1 2 2 2 3 3 3 4 5 11 12 22 32 45 52 322
+    shellSort.show(integers);//1 1 2 3 4 4 11 12 22 32 42 45 45 45 52 64 322
   }
 }
