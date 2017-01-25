5e2f5e96e909916b776ad5000967094eaea32331
sssvip <admin@dxscx.com>
2017-01-25 13:05:02 +0800

    feat: add notice method if array not was sorted

--- src/fundamentals/sort/mergesort/MergeSort.java
+++ src/fundamentals/sort/mergesort/MergeSort.java
@@ -67,5 +67,6 @@ public class MergeSort extends Sortable {
     mergeSort.show(integers);//1 4 2 4 2 4 5 44 2 3 2 1 43 1 23 12 3 1 23 34 2 4
     mergeSort.sort(integers);
     mergeSort.show(integers);//1 1 1 1 2 2 2 2 2 3 3 4 4 4 4 5 12 23 23 34 43 44
+    mergeSort.noticeIfNotSorted(integers);
   }
 }
