a252884e50e0c41c4aefb14b6b505bbc8453e0fa
sssvip <admin@dxscx.com>
2017-01-30 23:18:33 +0800

    feat: add simple frame of RedBlack BST

---
+++ src/fundamentals/tree/redblackbinarysearchtree/RedBlackBinarySearchTree.java
@@ -0,0 +1,19 @@
+package fundamentals.tree.redblackbinarysearchtree;
+
+
+public class RedBlackBinarySearchTree<Key extends Comparable<Key>, Value> {
+  private Node root;
+
+  private class Node {
+
+  }
+
+  private int size() {
+    return 0;
+  }
+
+  public void put(Key key, Value value) {
+
+  }
+
+}
