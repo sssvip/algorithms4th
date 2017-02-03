1cec095315cb19509bf09b717341496de1c1042a
sssvip <admin@dxscx.com>
2017-02-03 11:19:01 +0800

    feat: add basic node with color

--- src/fundamentals/tree/redblackbinarysearchtree/RedBlackBinarySearchTree.java
+++ src/fundamentals/tree/redblackbinarysearchtree/RedBlackBinarySearchTree.java
@@ -3,9 +3,29 @@ package fundamentals.tree.redblackbinarysearchtree;
 
 public class RedBlackBinarySearchTree<Key extends Comparable<Key>, Value> {
   private Node root;
+  private static final boolean RED = true;
+  private static final boolean BLACK = false;
 
   private class Node {
+    Key key;
+    Value value;
+    Node left, right;
+    int N;
+    boolean color;
 
+    public Node(Key key, Value value, int n, boolean color) {
+      this.key = key;
+      this.value = value;
+      N = n;
+      this.color = color;
+    }
+  }
+
+  private boolean isRed(Node node) {
+    if (node == null) {
+      return false;
+    }
+    return node.color == RED;
   }
 
   private int size() {
