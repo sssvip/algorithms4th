0412256edacb57184cddb98b61a7397328956291
sssvip <admin@dxscx.com>
2017-02-09 10:25:49 +0800

    feat: add depth first path implement and test it simply

--- src/fundamentals/graph/DepthFirstPaths.java
+++ src/fundamentals/graph/DepthFirstPaths.java
@@ -1,7 +1,68 @@
 package fundamentals.graph;
 
+
+import java.util.Stack;
+
 /**
- * Created by David-admin@dxscx.com on 2017/2/8.
+ * The type Depth first paths.
  */
 public class DepthFirstPaths {
+  //to store visit status of vertex
+  private boolean[] marked;
+  //
+  private int[] edgeTo;//key thinking
+  //start vertex
+  private final int s;
+
+  /**
+   * Instantiates a new Depth first paths.
+   *
+   * @param G the g
+   * @param s the s
+   */
+  public DepthFirstPaths(Graph G, int s) {
+    marked = new boolean[G.V()];
+    edgeTo = new int[G.V()];
+    this.s = s;
+    dfs(G, s);
+  }
+
+  private void dfs(Graph G, int v) {
+    marked[v] = true;
+    for (int w : G.adj(v)) {
+      if (!marked[w]) {
+        edgeTo[w] = v;
+        dfs(G, w);
+      }
+    }
+
+  }
+
+  /**
+   * Has path to boolean.
+   *
+   * @param v the v
+   * @return the boolean
+   */
+  public boolean hasPathTo(int v) {
+    return marked[v];
+  }
+
+  /**
+   * Path to iterable.
+   *
+   * @param v the v
+   * @return the iterable
+   */
+  public Iterable<Integer> pathTo(int v) {
+    if (!hasPathTo(v)) {
+      return null;
+    }
+    Stack<Integer> path = new Stack<Integer>();
+    for (int x = v; x != s; x = edgeTo[x]) {
+      path.push(x);
+    }
+    path.push(s);
+    return path;
+  }
 }
--- src/fundamentals/graph/DepthFirstPathsTest.java
+++ src/fundamentals/graph/DepthFirstPathsTest.java
@@ -1,7 +1,26 @@
 package fundamentals.graph;
 
-/**
- * Created by David-admin@dxscx.com on 2017/2/8.
- */
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class DepthFirstPathsTest {
+  @Test
+  public void testHasPathTo() {
+    Graph G = Graph.getTestGraphWithData();
+    //0 as the start vertex
+    DepthFirstPaths dfp = new DepthFirstPaths(G, 0);
+    Assert.assertTrue(dfp.hasPathTo(1));
+  }
+
+  @Test
+  public void testPathTo() {
+    Graph G = Graph.getTestGraphWithData();
+    //0 as the start vertex
+    DepthFirstPaths dfp = new DepthFirstPaths(G, 0);
+    //assert the path
+    Assert.assertEquals("[4, 5, 0]", dfp.pathTo(4).toString());
+    //assert unreachable
+    Assert.assertEquals(null, dfp.pathTo(9));
+  }
 }
